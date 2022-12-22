3e3e0c20fd7beb4f3ee9ed6c5b8fe3e5e580ef45
igorfduarte <igorf.duarte92@gmail.com>
2022-12-21 21:38:14 -0300

    frontend initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>} exact />
+            <Route path="/product/:id" element={<ProductScreen/>} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -7,9 +7,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest products</h1>
       <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
+        {products.map((product,index) => (
+          <Col key={index} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
