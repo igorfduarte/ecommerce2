3c625b819d23e3db760e4cff1e2cf94a4cc13aeb
igorfduarte <igorf.duarte92@gmail.com>
2022-12-22 09:37:21 -0300

    added server

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,13 +1,23 @@
-import React from "react";
-import products from "../products";
-import Product from '../components/Product'
+import React, { useState,useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import axios from 'axios'
+import Product from '../components/Product'
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    const fetchProducts = async()=>{
+      const {data} = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+  },[])
+
   return (
     <>
       <h1>Latest products</h1>
       <Row>
-        {products.map((product,index) => (
+        {products.map((product, index) => (
           <Col key={index} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host:true,
+    port:3000,
+    proxy:{
+      '/api':{
+        target: 'http://127.0.0.1:5000',
+        changeOrigin:true,
+        secure:false
+      }
+    }
+  }
 })
